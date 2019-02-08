added numerical gradient calcultation, fixed softmax

assignements/1/problem_1.py
@@ -6,10 +6,9 @@ from torchvision import transforms
 import matplotlib.pyplot as plt
 
 
-
 # TODO:
-# - Refactor forward and backward methods to generalize to L layers
-# - Add an extra dimension to W and X for the bias!
+# - HP search
+# - Complete check_grads?!
 
 
 class NN(object):
@@ -19,7 +18,8 @@ class NN(object):
                  output_size=10,
                  hidden_layers_size=[512, 1024],
                  init='zeros',
-                 lr=1.e-3):
+                 activation='sigmoid',
+                 lr=0.1):
 
         self.input_size = input_size
         self.hidden_layers_size = hidden_layers_size
@@ -29,6 +29,7 @@ class NN(object):
         self.init=init
 
         self._initialize_weights(init)
+        self._initialize_activation(activation)
 
     def _initialize_weights(self, init_method):
         def _random(neurons_in, neurons_out):
@@ -37,10 +38,11 @@ class NN(object):
             return np.zeros((neurons_in, neurons_out))
         def _normal(neurons_in, neurons_out):
             return np.random.normal(0, 1, (neurons_in, neurons_out))
-        def _glorot(neurons_in, neurons_out,low,high):
-            return np.random.uniform(low,high,(neurons_in, neurons_out))
+        def _glorot(neurons_in, neurons_out):
+            low = -np.sqrt(6 / (neurons_in + neurons_out))
+            high = np.sqrt(6 / (neurons_in + neurons_out))
+            return np.random.uniform(low, high, (neurons_in, neurons_out))
 
-        # Not super clean...
         init_weights = {
                 'random': _random,
                 'zeros': _zeros,
@@ -49,34 +51,56 @@ class NN(object):
                 }
 
         sizes = [self.input_size] + self.hidden_layers_size + [self.output_size]
-        if (init_method =='random') or (init_method =='zeros') or (init_method == 'normal'):
-            self.W = [init_weights[init_method](sizes[i], sizes[i+1]) for i in range(len(sizes) - 1 )]
-        else:
-            self.W = [init_weights[init_method](sizes[i], sizes[i+1], -np.sqrt(6/(sizes[i]+sizes[i+1])),np.sqrt(6/(sizes[i]+sizes[i+1]))) for i in range(len(sizes) - 1 )]
-
+        self.W = [init_weights[init_method](sizes[i], sizes[i+1]) for i in range(len(sizes) - 1 )]
         self.b = [np.zeros((1, neurons)) for neurons in sizes[1:]]
 
-    def _add_bias(self, h, W, b):
-        h = np.concatenate([h, np.ones((1, h.shape[1]))], axis=0)
-        W = np.concatenate([W, b])
-        return h, W
-
-    def forward(self, X):
-        h = X
-        cache = [(h, None)]
-        for W, b in zip(self.W, self.b):
-            # Add bias
-            hb, Wb = self._add_bias(h, W, b)
-            a = np.dot(Wb.T, hb)
-            h = self._sigmoid(a)
-            cache.append((h, a))
-        return h, cache
+    def _initialize_activation(self, activation):
+        if activation == 'sigmoid':
+            self.activation_f, self.activation_deriv_f = self._sigmoid, self._sigmoid_deriv
+        elif activation == 'tanh':
+            self.activation_f, self.activation_deriv_f = self._tanh, self._tanh_deriv
+        elif activation == 'linear':
+            self.activation_f, self.activation_deriv_f = self._linear, self._linear_deriv
 
     def _sigmoid(self, X):
          return 1 / (1 + np.exp(-X))
 
     def _sigmoid_deriv(self, X):
         return self._sigmoid(X) * (1 - self._sigmoid(X))
+
+    def _tanh(self, X):
+        return (np.exp(X) - np.exp(-X)) / (np.exp(X) + np.exp(-X))
+
+    def _tanh_deriv(self, X):
+        return 1 - self._tanh(X)**2
+
+    def _linear(self, X):
+        return X
+
+    def _linear_deriv(self, X):
+        return 1
+
+
+    def _add_bias(self, h, W, b):
+        h = np.concatenate([h, np.ones((1, h.shape[1]))], axis=0)
+        W = np.concatenate([W, b])
+        return h, W
+
+    def forward(self, X, model_W=None):
+        model_W = self.W if model_W is None else model_W
+        h = X
+        cache = [(h, None)]
+        for i, (W, b) in enumerate(zip(model_W, self.b)):
+            # Add bias
+            hb, Wb = self._add_bias(h, W, b)
+            a = np.dot(Wb.T, hb)
+            # Different activation function for last layer (softmax)
+            if i == len(model_W) - 1:
+                h = self._softmax(a)
+            else:
+                h = self.activation_f(a)
+            cache.append((h, a))
+        return h, cache
 
     def _softmax(self, X):
         """ Softmax activation function """
@@ -88,7 +112,7 @@ class NN(object):
         return -np.log((prediction * target).sum(axis=0)).mean()
 
     def backward(self, target, prediction, cache):
-        grads_W = []
+        grads = []
         grad_a = - (target - prediction)
         for i in range(len(cache) - 1):
             index = len(cache) - i - 2
@@ -96,20 +120,126 @@ class NN(object):
             grad_b = grad_a
             if index:
                 grad_h = np.dot(self.W[index], grad_a)
-                grad_a = np.multiply(grad_h, self._sigmoid_deriv(cache[index][1]))
-            grads_W.append(grad_W.T)
-        return [g for g in reversed(grads_W)]
+                grad_a = np.multiply(grad_h, self.activation_deriv_f(cache[index][1]))
+            grads.append((grad_W.T, np.sum(grad_b.T, axis=0)))
+        return [g for g in reversed(grads)]
 
     def update_weights(self, grads):
         if not self.train:
             raise Exception('You should not update weights while validating/testing')
-        self.W = [self.W[i] - (self.lr * grads[i]) for i in range(len(self.W))]
+        self.W = [self.W[i] - (self.lr * grads[i][0]) for i in range(len(self.W))]
+        self.b = [self.b[i] - (self.lr * grads[i][1]) for i in range(len(self.W))]
 
     def training(self):
         self.train = True
 
     def eval(self):
         self.train = False
+
+
+def plot_loss(loss_vector):
+    t = np.arange(loss_vector.size)
+    plt.ylabel('Average Loss')
+    plt.xlabel('Epoch')
+    plt.title('Initialization Effect')
+    plt.grid(True)
+    plt.xlim(0, 9)
+    plt.plot(t, loss_vector)
+    plt.legend(('Zero', 'Normal', 'Glorot'), loc='upper right')
+    plt.show()
+
+def get_accuracy(target, prediction):
+    res = np.argmax(target, axis=0) == np.argmax(prediction, axis=0)
+    return len(res[res]) / len(res)
+
+def check_grads(model, batch, p=1):
+    model_input, target = preprocess(batch)
+    # Only one example
+    model_input = model_input[:, :1]
+    target = target[:, :1]
+
+    prediction, cache = model.forward(model_input)
+    grads = model.backward(target, prediction, cache)
+
+    diff = []
+    legends = []
+    for k in range(5):
+        N = 10**k
+        num_grads = get_numerical_grads(model_input, target, model, N, p)
+        diff.append(np.max(grads[2][0][:p, :model.W[2].shape[1]] - num_grads[:p, :model.W[2].shape[1]]))
+        legends.append(f'N = {N}')
+
+#    plt.plot(diff)
+#    plt.legend(legends)
+#    plt.show()
+#    import pdb; pdb.set_trace()
+
+        for i in range(100, 100 + p):
+            for j in range(model.W[2].shape[1]):
+                # num_grad = 0 for all j != target!
+                print(f'{i},{j} grads {grads[2][0][i, j]}, num_grads {num_grads[i, j]}')
+
+
+def get_numerical_grads(X, y, model, N, p):
+    num_grad = np.zeros(model.W[2].shape)
+    perturb = np.zeros(model.W[2].shape)
+    e = 1 / N
+    for i in range(p):
+        for j in range(model.W[2].shape[1]):
+            perturb[i, j] = e
+            W = model.W.copy()
+            W[2] += perturb
+            loss2 = model.loss(model.forward(X, model_W=W)[0], y)
+            W[2] -= 2 * perturb
+            loss1 = model.loss(model.forward(X, model_W=W)[0], y)
+            num_grad[i, j] = (loss2 - loss1) / (2 * e)
+            perturb[i, j] = 0
+    return num_grad
+
+def train(model, trainset, validset, epochs):
+    loss_vector = np.zeros([epochs, 1])
+
+    best_accuracy = 0
+    best_W = None
+    best_b = None
+
+    for epoch in range(epochs):
+        # Training
+        loss = 0
+        model.training()
+        for i, batch in enumerate(trainset):
+            model_input, target = preprocess(batch)
+            prediction, cache = model.forward(model_input)
+            grads = model.backward(target, prediction, cache)
+            model.update_weights(grads)
+            loss += model.loss(prediction, target)
+        loss_vector[epoch, 0] = loss / (i+1)
+        print(f'Train loss={loss / (i + 1)} at epoch {epoch}')
+
+        # Validation
+        loss = 0
+        accuracy = 0
+        model.eval()
+        for i, batch in enumerate(validset):
+            model_input, target = preprocess(batch)
+            prediction, _ = model.forward(model_input)
+            loss += model.loss(prediction, target)
+            accuracy += get_accuracy(target, prediction)
+        print(f'Valid loss={loss / (i + 1)} at epoch {epoch}')
+        print(f'Valid accuracy={accuracy / (i+1)} at epoch {epoch}')
+
+        if accuracy / (i + 1) > best_accuracy:
+            best_accuracy = accuracy / (i + 1)
+            best_W = model.W.copy()
+            best_b = model.b.copy()
+
+    #for batch in trainset:
+    #    check_grads(model, batch)
+    #    break
+
+    model.W = best_W
+    model.b = best_b
+    return loss_vector
 
 
 def load_dataset(batch_size, data_path='./data'):
@@ -139,67 +269,22 @@ def preprocess(batch, n_class=10):
     target_one_hot[target, np.arange(target.shape[0])] = 1
     return model_input, target_one_hot
 
-def plot_loss(loss_vector):
-    
-    t = np.arange(loss_vector.size)
-    plt.ylabel('Average Loss')
-    plt.xlabel('Epoch')
-    plt.title('Initialization Effect')
-    plt.grid(True)
-    plt.xlim(0, 9)
-    plt.plot(t, loss_vector)
-    plt.legend(('Zero','Normal','Glorot'),loc='upper right')
-    plt.show()
-
-
-def main(model, trainset, validset, epochs):
-    loss_vector = np.zeros([epochs,1])
-
-    for epoch in range(epochs):
-        result=0
-
-        # Training
-        loss = 0
-        model.training()
-        for i, batch in enumerate(trainset):
-            model_input, target = preprocess(batch)
-            prediction, cache = model.forward(model_input)
-            grads = model.backward(target, prediction, cache)
-            model.update_weights(grads)
-            loss += model.loss(prediction, target)
-        loss_vector[epoch,0] = loss/(i+1)
-        print(f'Train loss={loss / (i + 1)} at epoch {epoch}')
-
-        # Validation
-        loss = 0
-        model.eval()
-        for i, batch in enumerate(validset):
-            model_input, target = preprocess(batch)
-            prediction, _ = model.forward(model_input)
-            loss += model.loss(prediction, target)
-            targ = np.argmax(np.asarray(target), axis=0)
-            pred = np.argmax(np.asarray(prediction), axis=0)
-            result += np.sum(np.equal(targ,pred))
-        print(f'Valid loss={loss / (i + 1)} at epoch {epoch}')
-        print(f'Valid accuracy={result/len((validset.dataset))} at epoch {epoch}')
-    return loss_vector
 
 if __name__ == '__main__':
-    epochs =2
+    epochs = 12
     batch_size = 128
-    Plot_loss_ = True
+    lr = 1.e-2
+    plot = False
+    init_methods = ['glorot']
+    # init_methods = ['zeros', 'normal', 'glorot']
 
-    model = NN()
     trainset, validset = load_dataset(batch_size)
-    
-    if Plot_loss_ == True:
-        init ={'zeros','normal', 'glorot'}
-        for inits in init:
-            model = NN(init=inits)
-            print(inits)
-            loss_vector=main(model=model,
-                             trainset=trainset,
-                             validset=validset,
-                             epochs=10)
-            plot_loss(loss_vector)
 
+    for init in init_methods:
+        model = NN(init=init, lr=lr)
+        loss_vector = train(model=model,
+                                trainset=trainset,
+                                validset=validset,
+                                epochs=epochs)
+    if plot:
+        plot_loss(loss_vector)